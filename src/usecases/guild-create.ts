6dc4f287ff20dbd7d3ea3051465cbb4dda12b344
https-dre <diaso.andre@outlook.com>
2025-04-03 16:42:12 -0300

    refatora a estrutura de repositórios e adiciona verificação de cargo automático ao adicionar membros ao guild

--- src/usecases/guild-create.ts
+++ src/usecases/guild-create.ts
@@ -1,9 +1,9 @@
 import { Guild } from "discord.js";
 import Pool from "../data/conn";
-import { insertGuild } from "../data/guild-repository";
+import { insertGuild } from "../repositories/guild-repository";
 import { deployCommands } from "../deploy-commands";
 
 export async function guild_create(guild: Guild) {
     await insertGuild(guild.id, guild.name);
-    await deployCommands({guildId: guild.id})
+    await deployCommands({ guildId: guild.id })
 }
--- src/usecases/guild-member-add.ts
+++ src/usecases/guild-member-add.ts
@@ -1,11 +1,52 @@
-import Pool from "../data/conn"
-import { find_build_by_id } from "../data/guild-repository"
+import { GuildMember } from "discord.js";
+import { verify_auto_role } from "../repositories/auto-role";
+import { find_build_by_id } from "../repositories/guild-repository"
 
-export async function guild_member_add(guild_id: string): Promise<boolean> {
-    const result = await find_build_by_id(guild_id);
-    if(result == null) {
-        return false;
+export async function guild_member_add(member: GuildMember): Promise<boolean> {
+    console.log(`${member.displayName} entrou no servidor ${member.guild.name}`)
+    const result = await find_build_by_id(member.guild.id);
+
+    if (result == null) {
         console.log("Guild não cadastrada!")
+        return false;
     }
-    return false
+
+    const has_auto_role = await verify_auto_role(member.guild.id);
+    console.log(has_auto_role);
+
+    if(!has_auto_role) {
+        return false;
+    }
+
+    const { guild } = member;
+
+    const role = guild.roles.cache.get(has_auto_role.role_id)
+    const botMember = guild.members.me;
+
+    if(!botMember) {
+        console.log("Bot não encontrado na guild!");
+        return false;
+    }
+
+    if(!role) {
+        return false;
+    }
+
+    if(member.roles.cache.get(role.id)) {
+        return false;
+    }
+
+    // Verifica se o cargo está acima do cargo mais alto do bot
+    if (role.position >= botMember.roles.highest.position) {
+        console.log(`O cargo "${role.name}" está acima do cargo mais alto do bot!`);
+        return false;
+    }
+
+    try {
+        await member.roles.add(role);
+    } catch (err) {
+        console.error(err);
+    }
+
+    return false;
 }
